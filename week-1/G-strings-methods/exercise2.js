18b20150aa510d64ca02913325196744ef071518
Anandamaya <arnoananda@gmail.com>
2020-04-03 23:16:34 +0200

    G exercise of the week 1 ready

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,6 @@
-var name = " Daniel  ";
+// Start by creating a variable `message`
+var name = "Daniel";
+var trimName = name.trim();
+var message = "My name is "+trimName+" and my name is "+trimName.length+" characters long";
 
 console.log(message);
